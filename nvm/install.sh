8babbb786b3e337e63f99dab351ff4233c9c04d3
Adrian Carriger <hello@adriancarriger.com>
2018-03-10 13:38:24 -0600

    feat(yarn): add yarn

--- nvm/install.sh
+++ nvm/install.sh
@@ -4,10 +4,17 @@
 #
 
 # Check for nvm
-if test ! $(which nvm)
+if [ ! -f $NVM_DIR/nvm.sh ];
 then
   echo "  Installing nvm for you."
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
   NVM_DIR="$HOME/.nvm" && . "$NVM_DIR/nvm.sh"
   nvm install --lts
+  npm install -g yarn
 fi
+
+# Install dotfile node dependencies
+yarn
+
+# Install global node modules
+yarn run global.install
