4d12721751e932129040822a919757566ac9932e
Adrian Carriger <hello@adriancarriger.com>
2018-03-11 07:14:50 -0500

    refactor(nvm): use nvm to manage global packages

--- nvm/install.sh
+++ nvm/install.sh
@@ -9,12 +9,7 @@ then
   echo "  Installing nvm for you."
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
   NVM_DIR="$HOME/.nvm" && . "$NVM_DIR/nvm.sh"
+  ln -s "$DOTFILES/nvm/default-packages" "$NVM_DIR/default-packages"
   nvm install --lts
-  npm install -g yarn
+  nvm install node
 fi
-
-# Install dotfile node dependencies
-yarn
-
-# Install global node modules
-yarn run global.install
