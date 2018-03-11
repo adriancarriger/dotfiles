f97e1724cbe1126bf3a7dc8fe3d77bd3534bb748
Adrian Carriger <hello@adriancarriger.com>
2018-03-11 07:14:50 -0500

    refactor(nvm): use nvm to manage global packages

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,8 +3,10 @@ if [ ! -n "$ZSH" ]; then
   ZSH=~/.oh-my-zsh
 fi
 
-# https://github.com/robbyrussell/oh-my-zsh/blob/master/tools/install.sh#L64-L67
-env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
-  printf "Error: git clone of oh-my-zsh repo failed\n"
-  exit 1
-}
+if [ ! -d $ZSH ]; then
+  # https://github.com/robbyrussell/oh-my-zsh/blob/master/tools/install.sh#L64-L67
+  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+    printf "Error: git clone of oh-my-zsh repo failed\n"
+    exit 1
+  }
+fi
