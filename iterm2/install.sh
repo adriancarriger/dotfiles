ef938ff8a5ece143a6499566cd02af82b6a592af
Adrian Carriger <hello@adriancarriger.com>
2018-03-12 18:33:29 -0500

    feat(iterm): add install script

---
+++ iterm2/install.sh
@@ -0,0 +1,11 @@
+ITERM_VERSION=3_1_5
+EXPECTED_SHA='7159ce6c96fe5c61653a6d6a9a45facfe4a9abff5bfa063e361efd70f89fd769'
+
+[ -e /Applications/iTerm.app ] && exit 0
+rm -rf ~/temp/iTerm2-$ITERM_VERSION.zip ~/temp/iTerm.app
+(mkdir -p ~/temp; cd $_; curl -O "https://iterm2.com/downloads/stable/iTerm2-$ITERM_VERSION.zip")
+actual_sha=$(shasum -a 256 ~/temp/iTerm2-$ITERM_VERSION.zip | awk '{print $1}')
+[ $EXPECTED_SHA != $actual_sha ] && exit 1
+unzip ~/temp/iTerm2-$ITERM_VERSION.zip -d ~/temp
+mv ~/temp/iTerm.app/ /Applications/
+rm -rf ~/temp/iTerm2-$ITERM_VERSION.zip
