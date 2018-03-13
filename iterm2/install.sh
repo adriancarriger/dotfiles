ab8fd7e7391d946097373f60264968bf318a156f
Adrian Carriger <hello@adriancarriger.com>
2018-03-13 07:14:56 -0500

    feat(iterm): update and install settings

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -9,3 +9,9 @@ actual_sha=$(shasum -a 256 ~/temp/iTerm2-$ITERM_VERSION.zip | awk '{print $1}')
 unzip ~/temp/iTerm2-$ITERM_VERSION.zip -d ~/temp
 mv ~/temp/iTerm.app/ /Applications/
 rm -rf ~/temp/iTerm2-$ITERM_VERSION.zip
+
+# Thanks! 👍 => http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
