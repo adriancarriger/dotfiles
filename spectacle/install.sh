2538e91119fa87edbb171d26478c2db89df5d608
Adrian Carriger <hello@adriancarriger.com>
2018-03-19 07:24:18 -0500

    fix(spectacle): only link config if installed

--- spectacle/install.sh
+++ spectacle/install.sh
@@ -2,6 +2,9 @@
 #
 # Spectacle
 
-if [ ! -L ~/Library/Application\ Support/Spectacle/shortcuts.json ]; then
-  ln -sf $DOTFILES/spectacle/shortcuts.json ~/Library/Application\ Support/Spectacle/shortcuts.json
-fi
+# Check if link is needed
+[ ! -d ~/Library/Application\ Support/Spectacle/shortcuts.json ] && exit 0
+[ -L ~/Library/Application\ Support/Spectacle/shortcuts.json ] && exit 0
+
+# Link config
+ln -sf $DOTFILES/spectacle/shortcuts.json ~/Library/Application\ Support/Spectacle/shortcuts.json
