9203dce95434cd60798bb87b28783758ea241ffb
Adrian Carriger <hello@adriancarriger.com>
2018-03-19 06:56:09 -0500

    fix(vscode): only symlink if needed

--- vscode/install-version.sh
+++ vscode/install-version.sh
@@ -4,15 +4,17 @@ CODE_VERSION=${CODE_VERSION:-code}
 [[ $CODE_VERSION == 'code' ]] && CODE_DIR=Code || CODE_DIR=Code\ -\ Insiders
 VSCODE_USER_PATH=~/Library/Application\ Support/$CODE_DIR/User
 
-if [ ! -L ~/Library/Application\ Support/$CODE_DIR/User/settings.json ]; then
-  # Backup current state
-  [ ! -d $DOTFILES/vscode/.backups/$CODE_VERSION ] && mkdir -p $DOTFILES/vscode/.backups/$CODE_VERSION/snippets
-  cp ~/Library/Application\ Support/$CODE_DIR/User/settings.json ~/.dotfiles/vscode/.backups/$CODE_VERSION/
-  cp ~/Library/Application\ Support/$CODE_DIR/User/keybindings.json ~/.dotfiles/vscode/.backups/$CODE_VERSION/
-  cp -r ~/Library/Application\ Support/$CODE_DIR/User/snippets/ ~/.dotfiles/vscode/.backups/$CODE_VERSION/
+# Check if install is needed
+[ ! -d ~/Library/Application\ Support/$CODE_DIR/User/settings.json ] && exit 0
+[ -L ~/Library/Application\ Support/$CODE_DIR/User/settings.json ] && exit 0
 
-  # Link config
-  ln -sf ~/.dotfiles/vscode/settings.json $VSCODE_USER_PATH/settings.json
-  ln -sf ~/.dotfiles/vscode/keybindings.json $VSCODE_USER_PATH/keybindings.json
-  [ ! -L "$VSCODE_USER_PATH/snippets" ] && ln -sf ~/.dotfiles/vscode/snippets/ $VSCODE_USER_PATH/snippets
-fi
+# Backup current state
+[ ! -d $DOTFILES/vscode/.backups/$CODE_VERSION ] && mkdir -p $DOTFILES/vscode/.backups/$CODE_VERSION/snippets
+cp ~/Library/Application\ Support/$CODE_DIR/User/settings.json ~/.dotfiles/vscode/.backups/$CODE_VERSION/
+cp ~/Library/Application\ Support/$CODE_DIR/User/keybindings.json ~/.dotfiles/vscode/.backups/$CODE_VERSION/
+cp -r ~/Library/Application\ Support/$CODE_DIR/User/snippets/ ~/.dotfiles/vscode/.backups/$CODE_VERSION/
+
+# Link config
+ln -sf ~/.dotfiles/vscode/settings.json $VSCODE_USER_PATH/settings.json
+ln -sf ~/.dotfiles/vscode/keybindings.json $VSCODE_USER_PATH/keybindings.json
+[ ! -L "$VSCODE_USER_PATH/snippets" ] && ln -sf ~/.dotfiles/vscode/snippets/ $VSCODE_USER_PATH/snippets
